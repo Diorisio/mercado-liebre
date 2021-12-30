f49e02361d3e14d5a00e4eb6d09f2181a00481ea
Diorisio <87914871+Diorisio@users.noreply.github.com>
2021-12-30 00:02:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path=require('path');
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT||3000, ()=>{
+    console.log('Servidor funcionando en el puerto:3000');
 });
 
 app.get('/', (req,res)=>{
